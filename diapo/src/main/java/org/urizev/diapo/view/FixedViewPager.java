8544835fa0ae4e86d6c0469dbdb931a1debbf655
Juan Carlos Vallejo <jcvallejo@start-app.es>
2016-03-01 15:21:41 +0100

    ViewPager bug worked around

---
+++ diapo/src/main/java/org/urizev/diapo/view/FixedViewPager.java
@@ -0,0 +1,29 @@
+package org.urizev.diapo.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by jcvallejo at 1/3/16.
+ */
+public class FixedViewPager extends ViewPager {
+    public FixedViewPager(Context context) {
+        super(context);
+    }
+
+    public FixedViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
