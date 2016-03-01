229cd31efbcc50c89f26b136b9e059f5e6bedcb4
Juan Carlos Vallejo <jcvallejo@start-app.es>
2016-03-01 14:55:32 +0100

    Immersive mode

--- diapo/src/main/java/org/urizev/diapo/DiapoActivity.java
+++ diapo/src/main/java/org/urizev/diapo/DiapoActivity.java
@@ -1,17 +1,33 @@
 package org.urizev.diapo;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import java.util.ArrayList;
 
 /**
- * Created by jcvallejo on 6/11/15.
+ * Created by jcvallejo at 6/11/15.
  */
 public class DiapoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        int flags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            flags |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            flags |= View.SYSTEM_UI_FLAG_IMMERSIVE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(flags);
 
         ArrayList<String> urls = getIntent().getStringArrayListExtra(Diapo.EXTRA_IMAGE_URLS);
         int index = getIntent().getIntExtra(Diapo.EXTRA_IMAGE_INDEX, -1);
